355cd17739fd3939d5db2f2af226e64a909f08c1
Anton Kuleshov <akbsit@yandex.ru>
2018-03-07 14:49:03 +0300

    update addOrderProperty.php

--- order/addOrderProperty.php
+++ order/addOrderProperty.php
@@ -1,1 +1,30 @@
 <?
+/**
+ * @param array $params
+ * @return bool
+ */
+function addOrderProperty($params = [])
+{
+    if ($params && !empty($params['order']) && !empty($params['code'])) {
+
+        $order = $params['order'];
+        $code  = $params['code'];
+        $value = !empty($params['value']) ? $params['value'] : '';
+
+        if (CModule::IncludeModule('sale')) {
+
+            if ($arProps = CSaleOrderProps::GetList([], ['CODE' => $code])->Fetch()) {
+
+                return CSaleOrderPropsValue::Add([
+                    'NAME'           => $arProps['NAME'],
+                    'CODE'           => $arProps['CODE'],
+                    'ORDER_PROPS_ID' => $arProps['ID'],
+                    'ORDER_ID'       => $order,
+                    'VALUE'          => $value
+                ]);
+            }
+        }
+    }
+
+    return false;
+}
