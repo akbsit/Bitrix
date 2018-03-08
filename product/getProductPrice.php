59e6eb2388a35484d22a6e6c068abbefd0ee1fab
Anton Kuleshov <akbsit@yandex.ru>
2018-03-08 10:04:18 +0300

    add getProductPrice.php

---
+++ product/getProductPrice.php
@@ -0,0 +1,30 @@
+<?
+/**
+ * @param array $params
+ * @return array
+ */
+function getProductPrice($params = [])
+{
+    $tmp = [];
+
+    if ($params && !empty($params['product'])) {
+
+        $product = $params['product'];
+
+        if (CModule::IncludeModule('catalog')) {
+
+            if ($price = CPrice::GetList([], ['PRODUCT_ID' => $product])->fetch()) {
+
+                $tmp['CURRENCY'] = $price['CURRENCY'];
+                $tmp['PRICE']    = $price['PRICE'];
+            }
+
+            if ($discount = CCatalogDiscount::GetDiscountByProduct($product)) {
+
+                $tmp['PRICE_DISCOUNT'] = CCatalogProduct::CountPriceWithDiscount($price['PRICE'], $price['CURRENCY'], $discount);
+            }
+        }
+    }
+
+    return $tmp;
+}
